5a03aff5422ed5e21a8c8ee2d0ba681d92984577
Guilherme Costa <guitavares1999@hotmail.com>
2021-04-05 21:07:49 -0300

    Projeto com Initializr

---
+++ Initializr/src/test/java/br/edu/ifsp/Initializr/InitializrApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifsp.Initializr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InitializrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
