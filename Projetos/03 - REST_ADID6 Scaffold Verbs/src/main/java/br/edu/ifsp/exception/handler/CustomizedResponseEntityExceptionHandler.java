6e57fb7e0f49d781ed03b82926fc02808616f42b
Guilherme Costa <guitavares1999@hotmail.com>
2021-05-03 00:05:00 -0300

    Adição de código para comunicar com banco de dados

--- Projetos/03 - REST_ADID6 Scaffold Verbs/src/main/java/br/edu/ifsp/exception/handler/CustomizedResponseEntityExceptionHandler.java
+++ Projetos/03 - REST_ADID6 Scaffold Verbs/src/main/java/br/edu/ifsp/exception/handler/CustomizedResponseEntityExceptionHandler.java
@@ -11,7 +11,7 @@ import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import br.edu.ifsp.exception.ExceptionResponse;
-import br.edu.ifsp.exception.UnsuportedMathOperationException;
+import br.edu.ifsp.exception.ResourceNotFoundException;
 
 @ControllerAdvice
 @RestController
@@ -28,7 +28,7 @@ public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExce
 		
 	}
 	
-	@ExceptionHandler(UnsuportedMathOperationException.class)
+	@ExceptionHandler(ResourceNotFoundException.class)
 	public final ResponseEntity<ExceptionResponse> 
 	              handleBadRequestExceptions(Exception ex, WebRequest request){
 		
