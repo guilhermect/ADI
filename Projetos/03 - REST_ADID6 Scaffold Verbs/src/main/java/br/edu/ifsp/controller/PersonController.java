b1d6cc8d5e6fc0cda7be32092f0b8c671d5b20b4
Guilherme Costa <guitavares1999@hotmail.com>
2021-05-17 21:54:52 -0300

    Versionamento

--- Projetos/03 - REST_ADID6 Scaffold Verbs/src/main/java/br/edu/ifsp/controller/PersonController.java
+++ Projetos/03 - REST_ADID6 Scaffold Verbs/src/main/java/br/edu/ifsp/controller/PersonController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.edu.ifsp.data.vo.PersonVO;
+import br.edu.ifsp.data.vo.v2.PersonVOV2;
 import br.edu.ifsp.services.PersonServices;
 
 @RestController
@@ -36,6 +37,11 @@ public class PersonController {
 	@PostMapping
 	public PersonVO create(@RequestBody PersonVO person) {
 		return services.create(person);
+	}
+	
+	@PostMapping("/v2")
+	public PersonVOV2 createV2(@RequestBody PersonVOV2 person) {
+		return services.createV2(person);
 	}
 	
 	@PutMapping
--- Projetos/03 - REST_ADID6 Scaffold Verbs/src/main/java/br/edu/ifsp/services/PersonServices.java
+++ Projetos/03 - REST_ADID6 Scaffold Verbs/src/main/java/br/edu/ifsp/services/PersonServices.java
@@ -6,8 +6,10 @@ import org.springframework.data.rest.webmvc.ResourceNotFoundException;
 import org.springframework.stereotype.Service;
 
 import br.edu.ifsp.converter.DozerConverter;
+import br.edu.ifsp.converter.custom.PersonConverter;
 import br.edu.ifsp.data.model.Person;
 import br.edu.ifsp.data.vo.PersonVO;
+import br.edu.ifsp.data.vo.v2.PersonVOV2;
 import br.edu.ifsp.repository.PersonRepository;
 
 @Service
@@ -15,12 +17,22 @@ public class PersonServices {
 	
 	@Autowired
 	PersonRepository repository;
+	
+	@Autowired
+	PersonConverter converter;
 		
 	public PersonVO create (PersonVO person) {
 		var entity = DozerConverter.parseObject(person, Person.class);
 		var vo = DozerConverter.parseObject(repository.save(entity), PersonVO.class);
 		return vo;
 	}
+	
+	public PersonVOV2 createV2 (PersonVOV2 person) {
+		var entity = converter.convertVOToEntity(person);
+		var vo = converter.convertEntitytoVO(repository.save(entity));
+		return vo;
+	}
+	
 	
 	public List<PersonVO> findAll(){
 		return DozerConverter.parseObjects(repository.findAll(), PersonVO.class);
